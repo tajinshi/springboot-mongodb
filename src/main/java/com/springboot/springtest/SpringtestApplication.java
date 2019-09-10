27cc3c756bae9c73e51c9373fb1486ea20c4b00c
tuojinshi <tuojinshi@navinfo.com>
2019-09-10 14:46:51 +0800

    添加注释

--- src/main/java/com/springboot/springtest/SpringtestApplication.java
+++ src/main/java/com/springboot/springtest/SpringtestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//这个@EnableSwagger2 注解可有可无，因为这Swagger2Config
 @EnableSwagger2
 
 @MapperScan(basePackages = {"com.springboot.springtest.dao"})
